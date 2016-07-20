e4253ecd14c7997649f0362ae53c377c625dced8
ctaw <tawcharlene@gmail.com>
2016-07-20 18:32:44 +0800

    final template for version1

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,6 +4,11 @@
 //= require turbolinks
 //= require_tree .
 
-//= require ./vendor/scrollreveal.min
+//= require ./vendor/wow
+//= require ./vendor/jquery.easing.min
+//= require ./vendor/scrolling-nav
+
 //= require ./vendor/jquery.magnific-popup.min
 //= require ./vendor/creative.min
+
+new WOW().init();
